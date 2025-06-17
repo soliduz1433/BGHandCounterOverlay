f1c493080d99e3fb19039115f2db9d84aa9f9e52
soliduz1433 <jon@widearea.ca>
2025-06-17 14:22:50 -0400

    Add files via upload

---
+++ BGHandCounterOverlay/Class1.cs
@@ -0,0 +1,53 @@
+﻿using Hearthstone_Deck_Tracker.Plugins;
+using Hearthstone_Deck_Tracker.API;
+using Hearthstone_Deck_Tracker.Hearthstone;
+using HearthDb.Enums;
+using System;
+using System.Windows.Controls;
+
+namespace BGHandCounterOverlay
+{
+    public class BGHandCounterOverlay : IPlugin
+    {
+        private HandCountOverlay _overlay;
+
+        public string Name => "BG Hand Counter";
+        public string Description => "Shows hand count in Battlegrounds";
+        public string ButtonText => "Settings";
+        public string Author => "YourName";
+        public Version Version => new Version(1, 0, 0, 0);
+        public MenuItem MenuItem => null;
+
+        public void OnLoad()
+        {
+            _overlay = new HandCountOverlay();
+        }
+
+        public void OnUnload()
+        {
+            _overlay?.Close();
+            _overlay = null;
+        }
+
+        public void OnButtonPress() { }
+
+        public void OnUpdate()
+        {
+            var gameType = Core.Game.CurrentGameType;
+            bool isBattlegrounds =
+                gameType == GameType.GT_BATTLEGROUNDS ||
+                gameType == GameType.GT_BATTLEGROUNDS_FRIENDLY;
+
+            if (isBattlegrounds)
+            {
+                int handCount = Core.Game.Player.HandCount;
+                if (!_overlay.IsVisible) _overlay.Show();
+                _overlay.HandCountText.Text = handCount.ToString();
+            }
+            else
+            {
+                if (_overlay?.IsVisible == true) _overlay.Hide();
+            }
+        }
+    }
+}
---
+++ BGHandCounterOverlay/HandCountOverlay.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace BGHandCounterOverlay
+{
+    public partial class HandCountOverlay : Window
+    {
+        public HandCountOverlay()
+        {
+            InitializeComponent();
+            // Attach the event handler in code
+            this.MouseLeftButtonDown += (sender, e) =>
+            {
+                if (e.ChangedButton == MouseButton.Left)
+                    this.DragMove();
+            };
+        }
+    }
+}
